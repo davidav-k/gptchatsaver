95c013ef4142fcfb7bdc3113b20c33c3848bbf5d
andrei_davidenko <davidenkav@gmail.com>
2025-02-21 17:20:26 +0500

    first commit

---
+++ src/test/java/com/example/gptchatsaver/GptchatsaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gptchatsaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GptchatsaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
