9745b8dd4934f8a3cfeea96d2b7119be7f7b4972
davidav <davidenkav@gmail.com>
2025-03-08 12:08:43 +0500

    refactoring code

--- src/main/java/com/example/gptchatsaver/repository/ChatMessageRepository.java
+++ src/main/java/com/example/gptchatsaver/repository/ChatMessageRepository.java
@@ -3,6 +3,7 @@ package com.example.gptchatsaver.repository;
 import com.example.gptchatsaver.entity.ChatMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 @Repository
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
 
-    @Query("SELECT c FROM ChatMessage c WHERE to_tsvector('russian', c.message) @@ plainto_tsquery(:query)")
-    List<ChatMessage> searchMessages(String query);
+    @Query(value = "SELECT * FROM chat_messages WHERE to_tsvector('russian', message) @@ plainto_tsquery(:query)",
+            nativeQuery = true)
+    List<ChatMessage> searchMessages(@Param("query") String query);
 }
